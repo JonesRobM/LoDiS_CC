7ff06e290ec48f939508958ffc86a2235e0c5be4
JonesRobM <52043020+JonesRobM@users.noreply.github.com>
2020-02-26 14:13:02 +0000

    Update input.py

--- Alpha/input.py
+++ Alpha/input.py
@@ -1,33 +1,20 @@
-from ExecuteTest import *
-
-"""
-Robert:
-    Change the arguments in the System dictionary to 
-    reflect the directory and files you are working with.
-"""
-
-"""
-
-Robert:
-    
-    This list below is just to inform you as to what quantities you can calculate.
-    
-"""
+import Process
+import pickle
 
 Supported=[
-        'euc', 'rdf', 'cna', 'adj', 'pdf', 'pdfhomo', 'agcn', 'nn', 'pos',
+        'rdf', 'cna', 'adj', 'pdf', 'pdfhomo', 'agcn', 'nn',
         'SimTime', 'EPot', 'ETot', 'EKin', 'EDelta', 'MeanETot', 'Temp'
            ]
 
 System = {
-        'base_dir' : '',
-        'movie_file_name' : 'PdMelt2-Movie.xyz',
-        'energy_file_name' : 'PdMelt2-En.out',
+        'base_dir' : '../../../Pd/1127/Melting100/',
+        'movie_file_name' : 'PdMelt5-Movie.xyz',
+        'energy_file_name' : 'PdMelt5-En.out',
         
         'Homo' : 'Cu', # Don't worry about this guy just yet, it will become relevant soon.
         
-        'Start' : 0, 'End' : None, 'Step' : 1, 'Skip' : 50,
-        'PdfStats' : True, 'HomoStats' : True, 'RdfStats' : True, 'CnaStats' : True,
+        'Start' : 0, 'End' : None, 'Step' : 1, 'Skip' : 50, 'UniformPDF' : False, 'Band' : 0.05,
+        #'PdfStats' : True, 'HomoStats' : True, 'RdfStats' : True, 'CnaStats' : True,
         
         #Below are quantities related to the energy file
         
@@ -51,8 +38,7 @@ Quantities = {
         'EKin' : None, 'EDelta' : None, 'MeanETot' : None, 'Temp' : None
         }
 
-metadata = Process(System, Quantities)
+TestMetadata = Process(System, Quantities)
 
-with open((System['base_dir']+'Meta2.csv'), 'w') as f:
-    for key in metadata.keys():
-        f.write("%s,%s\n"%(key,metadata[key]))
+with open('Metadata.csv', "wb") as f:
+    pickle.dump(TestMetaData,f, pickle.HIGHEST_PROTOCOL)
